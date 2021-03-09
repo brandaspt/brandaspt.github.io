b6ac89982b5a8591e4eb643181ecb23a2158d71c
brandas <tiagofpbrandao@gmail.com>
2021-03-09 11:10:24 +0100

    photos reduced. js cleaned up.

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,8 +9,8 @@ imgToExpand.forEach((element, index) => {
             expandedImg[0].src = this.src
             caption[0].innerText = this.alt
         } else {
-          expandedImg[1].src = this.src
-          caption[1].innerText = this.alt
+            expandedImg[1].src = this.src
+            caption[1].innerText = this.alt
         }
     })
 })
@@ -19,26 +19,13 @@ imgToExpand.forEach((element, index) => {
 const header = document.getElementsByTagName("header")[0]
 const nav = document.getElementsByClassName("top-nav-links")[0]
 const burger = document.getElementsByClassName("top-nav-burger")[0]
-const closeBtn = document.getElementsByClassName("close-button")[0]
-
-function toggleNav() {
-    if (window.innerWidth <= 789) {
-        header.classList.toggle("show-menu")
-    }
-}
 
 burger.addEventListener("click", function () {
     header.classList.toggle("show-menu")
 })
 
 nav.addEventListener("click", function () {
-    if (window.innerWidth <= 789) {
-        header.classList.toggle("show-menu")
-    }
-})
-
-window.addEventListener("resize", function () {
-    if (this.innerWidth > 789 && header.classList.contains("show-menu")) {
+    if (header.classList.contains("show-menu")) {
         header.classList.toggle("show-menu")
     }
 })
